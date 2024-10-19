aadc42fdc8ca0436525d63ca1f906e01715f6488
Priyanku <priyankubiswas85@gmail.com>
2024-10-19 11:31:51 +0200

    Add project files.

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,45 @@
+using Prime.Services;
+
+namespace PrimeServiceTests
+{
+    public class PrimeService_IsPrimeShould
+    {
+        #region Sample_TestCode
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            
+            var result = PrimeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+        #endregion
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void IsPrime_PrimesLessThan10_ReturnTrue(int value)
+        {
+            var result = PrimeService.IsPrime(value);
+
+            Assert.True(result, $"{value} should be prime");
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value)
+        {
+            var result = PrimeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,22 @@
+﻿namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public static bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+
+            for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
+            {
+                if (candidate % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
